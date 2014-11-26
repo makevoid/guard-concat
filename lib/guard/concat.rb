667fd7b92ea8e887e6dda0a8dd462147a980fceb
Ezekiel Gabrielse <ezekg@yahoo.com>
2014-11-26 11:49:14 -0600

    assign array for watchers var

--- lib/guard/concat.rb
+++ lib/guard/concat.rb
@@ -8,6 +8,7 @@ module Guard
     VERSION = '0.0.5'
 
     def initialize(opts={})
+      watchers = [] if !watchers
       @output = "#{opts[:output]}.#{opts[:type]}"
       files = opts[:files].join("|")
       regex = %r{^#{opts[:input_dir]}/(#{files})\.#{opts[:type]}$}
